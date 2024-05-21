834d8aab1414c29c0c44012f0e2801968b22ac76
Tag <sea15510@gmail.com>
2024-05-21 14:05:34 +0900

    [FEAT] SnapKit 기본 세팅
    -

--- SnapKitPractice/SnapKitPractice/ViewController.swift
+++ SnapKitPractice/SnapKitPractice/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var box = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(box)
+        box.backgroundColor = .green
+        box.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
     }
-
-
 }
-
