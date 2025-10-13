a91fc68d2f8b73705c3711795ba5dfe00f5dd331
Tag <sea15510@gmail.com>
2025-10-13 14:34:39 +0900

    [FEAT] TuistStudy 추가
    Tuist 기본적인 활용 및 설정법 학습

--- TuistTest/Project.swift
+++ TuistTest/Project.swift
@@ -5,12 +5,12 @@ let project = Project(
     name: "TuistSwiftUI",
     options: .options(developmentRegion: "ko"),
     packages: [
-        .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .upToNextMajor(from: "1.22.2")),
+        .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .upToNextMajor(from: "1.22.3")),
         .remote(url: "https://github.com/pointfreeco/swift-case-paths", requirement: .upToNextMajor(from: "1.3.0")),
     ],
     settings: .settings(
         base: [
-            "IPHONEOS_DEPLOYMENT_TARGET": "15.0",
+            "IPHONEOS_DEPLOYMENT_TARGET": "16.0",
             "SWIFT_VERSION": "5.0",
             "PRODUCT_NAME": "TuistSwiftUIApp"
         ]
@@ -21,24 +21,28 @@ let project = Project(
             destinations: .iOS,
             product: .app,
             bundleId: "Haesus.com.github.tuistswiftui",
-            infoPlist: .file(path: "App/Info.plist"),
+            infoPlist: .extendingDefault(with: [:]),
             sources: ["App/Sources/**"],
             resources: ["App/Resources/**"],
             entitlements: .file(path: "App/TuistSwiftUI.entitlements"),
             dependencies: [
-                .external(name: "Alamofire"),                                                                                                                                                                                                 
+                .external(name: "Alamofire"),
                 .external(name: "RxSwift"),
                 .external(name: "ReactorKit"),
                 .package(product: "ComposableArchitecture", type: .runtime),
                 .package(product: "CasePaths", type: .runtime),
 
                 // Macros (compile-time)
-//                .package(product: "ComposableArchitectureMacros", type: .macro),
-//                .package(product: "CasePathsMacros", type: .macro),
+                .package(product: "ComposableArchitectureMacros", type: .macro),
+                .package(product: "CasePathsMacros", type: .macro),
             ],
             settings: .settings(base: [
-                "DEVELOPMENT_TEAM": "S754RVHWQH",
-                "CODE_SIGN_STYLE": "Automatic"
+                "DEVELOPMENT_TEAM": "3TGR3P459K",
+                "CODE_SIGN_STYLE": "Automatic",
+                "INFOPLIST_KEY_CFBundleDisplayName": "TCA 연습",
+                "PRODUCT_NAME": "TCA_학습",
+                "MARKETING_VERSION": "1.0.0",
+                "CURRENT_PROJECT_VERSION": "42"
                 // 필요하면 아래도 추가 가능:
                 // "CODE_SIGN_IDENTITY": "Apple Development",
                 // "PROVISIONING_PROFILE_SPECIFIER": ""
